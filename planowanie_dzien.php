9fb0beeb4739343786a53c4fefb9eb2bea003fdf
Bartek_Plok <105823914+plokens27@users.noreply.github.com>
2025-05-19 19:26:14 +0200

    Add files via upload

--- planowanie_dzien.php
+++ planowanie_dzien.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twój planer</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="kontener">
 <?php
 require_once('polaczenie.php');
 
@@ -10,10 +20,17 @@ $sql = "INSERT INTO zadania (godzina_s, godzina_k, plan, kategoria) VALUES ('$go
 
 if (mysqli_query($polacz, $sql)){
     echo "Twój plan został pomyślnie dodany :)<br><br>
-    <a href='index.html'>Wróć do strony głównej</a><br><br>";
+    <a href='index.html'>Wróć do strony głównej</a> lub <a href='planer_dzienny.html'>dodaj kolejny plan.</a><br><br>";
 }else{
     echo "Błąd";
 }
 
 mysqli_close($polacz);
 ?>
+    </div>
+</body>
+</html>
+
+
+
+
